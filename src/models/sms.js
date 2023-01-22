4821175dfd648c483000ae77f72160c8caf4adcc
Frany-oss <francesco8bassino@gmail.com>
2023-01-22 12:28:05 -0500

    storing messages in db

---
+++ src/models/sms.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+const newSchema = new Schema({
+    Body: {
+        type: String,
+        require: true,
+    },
+    To: {
+        type: String,
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = model('sms', newSchema);
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,14 +1,23 @@
 const {Router} = require('express')
 const router = Router();
 const {sendMessage} = require('../twilio/send-sms');
+const SMS = require('../models/sms')
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
 router.post('/send-sms', async (req, res) => {
-    const response = await sendMessage(req.body.message, req.body.phone);
-    res.send("received")
+
+    const {message, phone} = req.body;
+    if(!message || !phone) return res.json('Missing message or phone');
+
+    const result = await sendMessage(req.body.message, req.body.phone);
+    console.log(result.sid)
+    
+    await SMS.create({Body: req.body.message, To: req.body.phone });
+
+    res.redirect('/');
 
 });
 
